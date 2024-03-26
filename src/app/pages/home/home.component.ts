52ceb6bf3d6fbb1cac5fb9e2e09eb4d1b1700c5f
bravoagustin <bravoagustin149@gmail.com>
2024-03-26 17:01:31 -0300

    first commit

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,49 @@
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Resultado } from 'src/app/interfaces/pokeapi';
+import { PokemonService } from 'src/app/services/pokemon.service';
+
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  constructor(private pokemonService: PokemonService){}
+
+  //Variable que hace referencia al elemento #tarjetas
+  @ViewChild('tarjetas') tarjetasElement!:ElementRef;
+
+  //la lista se va a llamar con los elementos ya existentes y con la nueva llamada de la Api
+  listaPokemon:Resultado[] = []
+
+  pagina:number = 1;
+  cargando:boolean = false;
+
+  ngOnInit(): void {
+    this.cargarLista();
+  }
+
+  async cargarLista(){
+    
+    this.cargando = true;
+    //todos los elementos de la lista
+    this.listaPokemon = [...this.listaPokemon, ...await this.pokemonService.getByPage(this.pagina)];
+    console.log(this.listaPokemon)
+    this.cargando = false;
+    this.pagina++;
+
+  }
+  
+  //evento
+  onScroll(e:any){
+    if(this.cargando) return;
+    if(
+      Math.round(
+        this.tarjetasElement.nativeElement.clientHeight + this.tarjetasElement.nativeElement.scrollTop)
+        === e.srcElement.scrollHeight){
+          this.cargarLista();
+        }
+      }
+  }
---
+++ src/app/services/pokemon.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { Resultado } from '../interfaces/pokeapi';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PokemonService {
+
+  constructor() { }
+
+  async getByPage(page:number, size: number = 40): Promise<Resultado[]>{
+    if(page > 5) return [];
+    const offset = size*(page-1);
+    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/?limit=${size}&offset=${offset}`);
+    const resJson = await res.json();
+    if(resJson.results.length > 0) return resJson.results
+    return [];
+  }
+
+  async getById(id:string){
+    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+    const resJson = await res.json();
+    console.log(resJson);
+  }
+
+  getDescription(){
+
+  }
+}
